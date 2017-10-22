fc518c9988b11842ba84b0dbde6f00473a3f3d6b
PhanTanPhuc <phantanphuc2512@gmail.com>
2017-10-22 20:36:12 +0700

    - delete BB gen

--- pretrain/getBBGT/parse_test.bat
+++
@@ -1,2 +0,0 @@
-python get_training_set.py ./test_inkml ./../pretraindataconfig/filepath_test test
-pause
--- pretrain/getBBGT/parse_train.bat
+++
@@ -1,2 +0,0 @@
-python get_training_set.py ./train_inkml ./../pretraindataconfig/filepath_train train
-pause
