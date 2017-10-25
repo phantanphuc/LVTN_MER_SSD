587a997bec75c4a82deb5427f0e7c5e860211dab
PhanTanPhuc <phantanphuc2512@gmail.com>
2017-10-25 07:07:20 +0700

    - add debug

--- pretrain/train.py
+++ pretrain/train.py
@@ -14,6 +14,7 @@ import torchvision
 import torchvision.transforms as transforms
 
 from ssd import VGGPretrain
+from ssd_new import vgg16_bn
 #from utils import progress_bar
 from datagen import ListDataset
 
@@ -27,7 +28,7 @@ import matplotlib.pyplot as plt
 p_batch_size = 2
 Continue_training = False
 lr = 0.001
-epoch_count = 3
+epoch_count = 30
 ########################################################################
 
 
@@ -48,15 +49,21 @@ print('==> Preparing data..')
 transform = transforms.Compose([transforms.ToTensor(),
 								transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
 
-trainset = ListDataset(root='./zipdataset/train/', list_file='./pretraindataconfig/train_path.txt', transform=transform)
+trainset = ListDataset(root='./dataset/train/', list_file='./pretraindataconfig/train_patha.txt', transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=p_batch_size, shuffle=True)
 
-testset = ListDataset(root='./zipdataset/test/', list_file='./pretraindataconfig/test_path.txt', transform=transform)
+testset = ListDataset(root='./dataset/test/', list_file='./pretraindataconfig/train_patha.txt', transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=p_batch_size, shuffle=False)
 
 
 # Model
-net = VGGPretrain()
+
+
+if True:
+	net = VGGPretrain()
+else:
+	net = 
+
 if Continue_training:
 	print('==> Resuming from checkpoint..')
 	checkpoint = torch.load('./checkpoint/ckpt0.pth')
@@ -79,6 +86,24 @@ if use_cuda:
 optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
 
 
+def try_print(print_flag = True):
+	params = [p for p in list(net.parameters()) if p.requires_grad==True]
+	for p in params:
+		p_grad = p.grad 
+		
+		try:
+			if print_flag:
+				print ('exist')
+				print (type(p_grad))
+				print (p_grad.data.numpy().shape)
+			else:
+				print (p_grad.data.numpy())
+					
+		except:
+			if print_flag:
+				print ('non - exist')
+				pass
+
 
 # Training
 def train(epoch):
@@ -96,7 +121,7 @@ def train(epoch):
 		images = Variable(images)
 		label = Variable(label)
 
-#		pdb.set_trace()
+
 		optimizer.zero_grad()
 		pred = net(images)
 #		pdb.set_trace()
@@ -104,13 +129,13 @@ def train(epoch):
 		
 		loss = criterion(pred, label)
 		
-		
+		try_print()
 		loss.backward()
 		optimizer.step()
 
 		train_loss += loss.data[0]
 #		pdb.set_trace()
-		if train_ite%1==0:
+		if train_ite%1==2:
 			all_loss.append(train_loss/(batch_idx+1))
 			plt.ion()
 			plt.plot(all_loss)
@@ -140,7 +165,7 @@ def test(epoch):
 		
 		loss = criterion(pred, label)
 		test_loss += loss.data[0]
-		if test_ite%10==9:
+		if test_ite%1==3:
 			all_testloss.append(test_loss/(batch_idx+1))
 			plt.ion()
 			plt.plot(all_testloss)
