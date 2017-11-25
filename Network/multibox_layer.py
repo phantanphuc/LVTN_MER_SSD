ce1257ab4cdd49900edf4b16c0106f92bb0dc0f5
PhanTanPhuc <phantanphuc2512@gmail.com>
2017-11-25 20:53:01 +0700

    - refactor tab

--- Network/multibox_layer.py
+++ Network/multibox_layer.py
@@ -14,57 +14,57 @@ from NetworkConfig import *
 
 
 class MultiBoxLayer(nn.Module):
-    num_classes = args.class_count
+	num_classes = args.class_count
 
-    if Network_type == 0:
-        num_anchors = [4,6,6,6,4,4]
-        in_planes = [512,1024,512,256,256,256]
+	if Network_type == 0:
+		num_anchors = [4,6,6,6,4,4]
+		in_planes = [512,1024,512,256,256,256]
 
-    elif Network_type == 1:
-        num_anchors = [4,6,6,6,4,4,4]
-        in_planes = [512,1024,512,256,256,256,256]
+	elif Network_type == 1:
+		num_anchors = [4,6,6,6,4,4,4]
+		in_planes = [512,1024,512,256,256,256,256]
 
-    elif Network_type == 2:
-        num_anchors = [4,6,6,6,4,4,4]
-        in_planes = [512,1024,512,256,256,256,256]
+	elif Network_type == 2:
+		num_anchors = [4,6,6,6,4,4,4]
+		in_planes = [512,1024,512,256,256,256,256]
 
-    elif Network_type == 3:
-        num_anchors = [4,6,6,6,6,4,4,4]
-        in_planes = [512,1024,512,256,256,256,256, 256]
+	elif Network_type == 3:
+		num_anchors = [4,6,6,6,6,4,4,4]
+		in_planes = [512,1024,512,256,256,256,256, 256]
 
 
-    def __init__(self):
-        super(MultiBoxLayer, self).__init__()
+	def __init__(self):
+		super(MultiBoxLayer, self).__init__()
 
-        self.loc_layers = nn.ModuleList()
-        self.conf_layers = nn.ModuleList()
-        for i in range(len(self.in_planes)):
-        	self.loc_layers.append(nn.Conv2d(self.in_planes[i], self.num_anchors[i]*4, kernel_size=3, padding=1))
-        	self.conf_layers.append(nn.Conv2d(self.in_planes[i], self.num_anchors[i] * args.class_count, kernel_size=3, padding=1))
+		self.loc_layers = nn.ModuleList()
+		self.conf_layers = nn.ModuleList()
+		for i in range(len(self.in_planes)):
+			self.loc_layers.append(nn.Conv2d(self.in_planes[i], self.num_anchors[i]*4, kernel_size=3, padding=1))
+			self.conf_layers.append(nn.Conv2d(self.in_planes[i], self.num_anchors[i] * args.class_count, kernel_size=3, padding=1))
 
-    def forward(self, xs):
-        '''
-        Args:
-          xs: (list) of tensor containing intermediate layer outputs.
+	def forward(self, xs):
+		'''
+		Args:
+		  xs: (list) of tensor containing intermediate layer outputs.
 
-        Returns:
-          loc_preds: (tensor) predicted locations, sized [N,8732,4].
-          conf_preds: (tensor) predicted class confidences, sized [N,8732,21].
-        '''
-        y_locs = []
-        y_confs = []
-        for i,x in enumerate(xs):
-            y_loc = self.loc_layers[i](x)
-            N = y_loc.size(0)
-            y_loc = y_loc.permute(0,2,3,1).contiguous()
-            y_loc = y_loc.view(N,-1,4)
-            y_locs.append(y_loc)
+		Returns:
+		  loc_preds: (tensor) predicted locations, sized [N,8732,4].
+		  conf_preds: (tensor) predicted class confidences, sized [N,8732,21].
+		'''
+		y_locs = []
+		y_confs = []
+		for i,x in enumerate(xs):
+			y_loc = self.loc_layers[i](x)
+			N = y_loc.size(0)
+			y_loc = y_loc.permute(0,2,3,1).contiguous()
+			y_loc = y_loc.view(N,-1,4)
+			y_locs.append(y_loc)
 
-            y_conf = self.conf_layers[i](x)
-            y_conf = y_conf.permute(0,2,3,1).contiguous()
-            y_conf = y_conf.view(N,-1, args.class_count)
-            y_confs.append(y_conf)
+			y_conf = self.conf_layers[i](x)
+			y_conf = y_conf.permute(0,2,3,1).contiguous()
+			y_conf = y_conf.view(N,-1, args.class_count)
+			y_confs.append(y_conf)
 
-        loc_preds = torch.cat(y_locs, 1)
-        conf_preds = torch.cat(y_confs, 1)
-        return loc_preds, conf_preds
+		loc_preds = torch.cat(y_locs, 1)
+		conf_preds = torch.cat(y_confs, 1)
+		return loc_preds, conf_preds
